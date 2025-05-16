a4c9d059865e02fbda52d427ebbe179fd0bc138a
news782 <cartinmaubert@gmail.com>
2025-05-16 18:04:04 +0200

    Correction de la classe Monstre.java dans l'optique de faire une correction/ refonte du projet car mauvaise gestion de certains éléments

--- src/monstres/Monstre.java
+++ src/monstres/Monstre.java
@@ -1,71 +1,43 @@
 package monstres;
 
+
 public class Monstre {
     private String espece;
-    private int numero; // utile s'il y a plusieurs monstres de la même espèce
-    // Caractéristiques
+    private int numero;
+    private String attaqueNom;
+    private int porteeAttaque;
+    private String degatsAttaque;
     private int pointsDeVie;
     private int force;
     private int dexterite;
     private int classeArmure;
     private int initiative;
-    // Attaque
-    private int porteeAttaque;
-    private int degatsAttaque;
 
-    public Monstre(String espece, int numero, int pointsDeVie, int force, int dexterite,
-                   int classeArmure, int initiative, int porteeAttaque, int degatsAttaque) {
+    public Monstre(String espece, int numero, String attaqueNom, int porteeAttaque, String degatsAttaque,
+                   int pointsDeVie, int force, int dexterite, int classeArmure, int initiative) {
         this.espece = espece;
         this.numero = numero;
+        this.attaqueNom = attaqueNom;
+        this.porteeAttaque = porteeAttaque;
+        this.degatsAttaque = degatsAttaque;
         this.pointsDeVie = pointsDeVie;
         this.force = force;
         this.dexterite = dexterite;
         this.classeArmure = classeArmure;
         this.initiative = initiative;
-        this.porteeAttaque = porteeAttaque;
-        this.degatsAttaque = degatsAttaque;
     }
 
-    public void subirDegats(int degats) {
-        pointsDeVie -= degats;
-        System.out.println(getNomComplet() + " subit " + degats + " points de dégâts !");
-    }
-
-    public String getNomComplet() {
-        return espece + " #" + numero;
-    }
-
-    public int getPointsDeVie() {
-        return pointsDeVie;
-    }
-
-    public int getForce() {
-        return force;
-    }
-
-    public int getDexterite() {
-        return dexterite;
-    }
-
-    public int getClasseArmure() {
-        return classeArmure;
-    }
-
-    public int getInitiative() {
-        return initiative;
-    }
-
-    public int getPorteeAttaque() {
-        return porteeAttaque;
-    }
-
-    public int getDegatsAttaque() {
-        return degatsAttaque;
-    }
-
-    public boolean estVivant() {
-        return pointsDeVie > 0;
-    }
+    public String getNom() {return espece + " " + numero;}
+    public String getEspece() {return espece;}
+    public int getNumero() {return numero;}
+    public String getAttaqueNom() {return attaqueNom;}
+    public int getPorteeAttaque() {return porteeAttaque;}
+    public String getDegatsAttaque() {return degatsAttaque;}
+    public int getPointsDeVie() {return pointsDeVie;}
+    public int getForce() {return force;}
+    public int getDexterite() {return dexterite;}
+    public int getClasseArmure() {return classeArmure;}
+    public int getInitiative() {return initiative;}
 }
 
 
