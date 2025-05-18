15b64fbeab4b0e639ca7b7395c3a1bc7a88e3c74
julot <julot424@gmail.com>
2025-05-18 14:53:06 +0200

    Correction Nom attributs

--- src/monstres/Monstre.java
+++ src/monstres/Monstre.java
@@ -30,16 +30,27 @@ public class Monstre {
     }
 
     public String getNom() {return espece + " " + numero;}
+
     public String getEspece() {return espece;}
+
     public int getNumero() {return numero;}
+
     public String getAttaqueNom() {return attaqueNom;}
+
     public int getPorteeAttaque() {return porteeAttaque;}
+
     public String getDegatsAttaque() {return degatsAttaque;}
+
     public int getPointsDeVie() {return pointsDeVie;}
+
     public int getForce() {return force;}
+
     public int getDexterite() {return dexterite;}
+
     public int getClasseArmure() {return classeArmure;}
+
     public int getInitiative() {return initiative;}
+
     public int getPointsDeVieMax(){return pdvMax;}
     //string getPointsDeVieMax à implémenter
 
