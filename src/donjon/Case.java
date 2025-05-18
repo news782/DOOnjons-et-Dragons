7e446b1d09ba3ef7f1ee9c115f6db63c292e8141
julot <julot424@gmail.com>
2025-05-18 13:16:12 +0200

    Correction Nom attributs

--- src/donjon/Case.java
+++ src/donjon/Case.java
@@ -5,32 +5,32 @@ import monstres.Monstre;
 import personnages.Personnage;
 
 public class Case {
-    private int x,y;
-    private boolean obstacle;
-    private Monstre monstre;
-    private Equipement equipement;
-    private Personnage joueur;
+    private int m_x,m_y;
+    private boolean m_obstacle;
+    private Monstre m_monstre;
+    private Equipement m_equipement;
+    private Personnage m_joueur;
 
     public Case(int x, int y, boolean obstacle) {
-        this.x=x;
-        this.y=y;
-        this.obstacle=obstacle;
+        this.m_x=x;
+        this.m_y=y;
+        this.m_obstacle =obstacle;
     }
 
-    public boolean estLibre() {return !obstacle && monstre==null;}
-    public void setObstacle(boolean obstacle) {this.obstacle=obstacle;}
-    public void setMonstre(Monstre monstre) {this.monstre=monstre;}
-    public void setJoueur(Personnage joueur) {this.joueur=joueur;}
-    public void setEquipement(Equipement equipement) {this.equipement=equipement;}
+    public boolean estLibre() {return !m_obstacle && m_monstre ==null;}
+    public void setM_obstacle(boolean m_obstacle) {this.m_obstacle = m_obstacle;}
+    public void setM_monstre(Monstre m_monstre) {this.m_monstre = m_monstre;}
+    public void setJoueur(Personnage joueur) {this.m_joueur =joueur;}
+    public void setM_equipement(Equipement m_equipement) {this.m_equipement = m_equipement;}
     public Personnage getJoueur(Personnage joueur) {return joueur;}
     public Monstre getMonstre(Monstre monstre) {return  monstre;}
 
     @Override
     public String toString() {
-        if (obstacle) {return "# ";}         // Obstacle présent
-        if (monstre != null) {return "M ";}     // Monstre présent
-        if (equipement != null) {return "E ";}  // Équipement présent
-        if (joueur != null) {return "J ";}      // Joueur présent
+        if (m_obstacle) {return "# ";}         // Obstacle présent
+        if (m_monstre != null) {return "M ";}     // Monstre présent
+        if (m_equipement != null) {return "E ";}  // Équipement présent
+        if (m_joueur != null) {return "J ";}      // Joueur présent
         return ". ";                            // Case vide
     }
 }
